864a4e94e25727c7ae67b2303a8447055cdce6f8
geypidr <156920473+geypidr@users.noreply.github.com>
2024-01-17 17:44:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
- for i in range(0, 10, 1):
-      print(i)
+stroka = input()
+counter_l = 0
+for i in stroka:
+    if i == '(':
+        counter_l += 1
+        print("yes")
+    else:
+        print("no")
+
+
+
+
+
+
+
 
 
 
